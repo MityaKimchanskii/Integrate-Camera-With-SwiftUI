534bb7bb9a55198c6033fd0d45c58446150b78a3
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-19 12:29:17 -0700

    Initial Commit

---
+++ Integrate-Camera-With-SwiftUI/Integrate_Camera_With_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Integrate_Camera_With_SwiftUIApp.swift
+//  Integrate-Camera-With-SwiftUI
+//
+//  Created by Mitya Kim on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Integrate_Camera_With_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
