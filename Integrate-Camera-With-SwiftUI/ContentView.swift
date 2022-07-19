3389236b2884c08b1daaeac29fecb743e9b49e79
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-19 14:56:32 -0700

    final commit

--- Integrate-Camera-With-SwiftUI/ContentView.swift
+++ Integrate-Camera-With-SwiftUI/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showImagePicker: Bool = false
+    @State private var image: Image? = nil
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            image?.resizable()
+                .scaledToFit()
+            
+            Button {
+                self.showImagePicker = true
+            } label: {
+                Text("Open Camera")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(.green)
+                    .cornerRadius(10)
+            }
+            
+            
+        }.sheet(isPresented: self.$showImagePicker) {
+            PhotoCaptureView(showImagePicker: self.$showImagePicker, image: self.$image)
+        }
     }
 }
 
---
+++ Integrate-Camera-With-SwiftUI/PhotoCaptureView.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCaptureView.swift
+//  Integrate-Camera-With-SwiftUI
+//
+//  Created by Mitya Kim on 7/19/22.
+//
+
+import SwiftUI
+
+struct PhotoCaptureView: View {
+    
+    @Binding var showImagePicker: Bool
+    @Binding var image: Image?
+    
+    var body: some View {
+        ImagePicker(isShown: $showImagePicker, image: $image)
+    }
+}
+
+struct PhotoCaptureView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoCaptureView(showImagePicker: .constant(false), image: .constant(Image("")))
+    }
+}
